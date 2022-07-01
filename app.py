fd8d3c74a92c2a71e0025be822fb181021c5b45b
B0806218 <108395225+B0806218@users.noreply.github.com>
2022-07-01 10:31:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ def predict():
    
     ####################### FROM AUTOAI DEPLOYMENT API #######################
     # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-    API_KEY = "JFEp-T-YU4Wsdf5bxxTILX1avq0HTCdO0t4P_EMt-RNm"
+    API_KEY = "ApiKey-33d8b275-89fb-48ad-a39b-dcb9599de6ba"
     token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
-     API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
     mltoken = token_response.json()["access_token"]
 
     header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
